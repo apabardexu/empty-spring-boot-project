97b688b1477fc2375791e5d8207ad969c6e1d351
rhs <apabardexu@gmail.com>
2025-02-21 23:40:40 +0100

    Añadido comentario

--- src/main/java/com/empty/EmptySpringBootProjectApplication.java
+++ src/main/java/com/empty/EmptySpringBootProjectApplication.java
@@ -3,6 +3,8 @@ package com.empty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Add comment
+
 @SpringBootApplication
 public class EmptySpringBootProjectApplication {
 
