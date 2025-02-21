f04194579b5ac7475f1da718548e049e0eb19bf1
rhs <apabardexu@gmail.com>
2025-02-21 22:50:37 +0100

    Proyecto vacío

---
+++ src/main/java/com/empty/EmptySpringBootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.empty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmptySpringBootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmptySpringBootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empty/EmptySpringBootProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmptySpringBootProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
